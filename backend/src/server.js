d85fe056fe630155836f922790fd2df46c4e6483
Suttikarn Chaichana <suttikarnchaichana@hotmail.com>
2025-12-27 18:57:43 +0700

    edit package

--- backend/src/server.js
+++ backend/src/server.js
@@ -13,7 +13,7 @@ app.get("/api/health", (req, res) => {
 // make our app ready for deployment
 
 if (ENV.NODE_ENV == "production"){
-    app.use(express.static(path.join(__dirname,"../admin/dist")))
+    app.use(express.static(path.join(__dirname,"../admin","dist")));
 
     app.get("/{*any}",(rea,res) => {
         res.sendFile(path.join(__dirname,"../admin","dist","index.html"));
