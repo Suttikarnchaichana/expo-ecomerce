9e834159dbe77be7b722e134e154f6c4abe8133b
Suttikarn Chaichana <suttikarnchaichana@hotmail.com>
2025-12-27 22:48:45 +0700

    edit

--- backend/src/config/inngest.js
+++ backend/src/config/inngest.js
@@ -2,26 +2,26 @@ import { Inngest } from "inngest";
 import { connectDB } from "./db.js";
 import { User } from "../models/user.model.js";
 
-
-export const inngest = new Inngest({id: "ecommerce-app"});
+export const inngest = new Inngest({ id: "ecommerce-app" });
 
 const syncUser = inngest.createFunction(
-  {id: "sync-user"},
-  {event: "clerk/user.created"}, 
+  { id: "sync-user" },
+  { event: "clerk/user.created" },
   async ({ event }) => {
     await connectDB();
     const { id, email_addresses, first_name, last_name, image_url } = event.data;
 
     const newUser = {
-        clerkId: id,
-        email: email_addresses[0]?.email_address,
-        name: `${first_name} ${last_name}` || "User",
-        imageUrl: image_url,
-        addressess: [],
-        wishlist: [],
+      clerkId: id,
+      email: email_addresses[0]?.email_address,
+      name: `${first_name || ""} ${last_name || ""}` || "User",
+      imageUrl: image_url,
+      addresses: [],
+      wishlist: [],
     };
+
     await User.create(newUser);
-  } 
+  }
 );
 
 const deleteUserFromDB = inngest.createFunction(
